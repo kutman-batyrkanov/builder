787e9a25d86d6a8ad9bdafe8c81d33bba0445f8c
Your Name <you@example.com>
2021-03-19 15:21:54 +0600

    added drawer js component

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,15 +1,26 @@
 import classes from "./Drawer.module.css";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import Backdrop from "../UI/Backdrop/Backdrop";
+import { useState } from "react";
 
+const Drawer = ({}) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-const Drawer = ({  }) => {
-    return ( 
-        <div className={classes.Drawer}>
-            <Logo />
-            <Nav />
-        </div>
-     );
-}
- 
+  const drawerClasses = [
+    classes.content,
+    drawerOpen ? classes.open : classes.close
+  ];
+
+  return (
+    <div className={classes.Drawer}>
+        <Backdrop show={drawerOpen} />
+      <div className={drawerClasses.join(" ")}>
+        <Logo />
+        <Nav />
+      </div>
+    </div>
+  );
+};
+
 export default Drawer;

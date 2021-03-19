4c4211c9d6548862f8139155b2222d147dd2a3fb
Your Name <you@example.com>
2021-03-19 15:58:09 +0600

    drawer js and drwer css added style

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -2,19 +2,17 @@ import classes from "./Drawer.module.css";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import Backdrop from "../UI/Backdrop/Backdrop";
-import { useState } from "react";
 
 const Drawer = ({}) => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const drawerClasses = [
     classes.content,
-    drawerOpen ? classes.open : classes.close
+    open ? classes.open : classes.close
   ];
 
   return (
     <div className={classes.Drawer}>
-        <Backdrop show={drawerOpen} />
+        <Backdrop show={open} click={closeDrawer} />
       <div className={drawerClasses.join(" ")}>
         <Logo />
         <Nav />

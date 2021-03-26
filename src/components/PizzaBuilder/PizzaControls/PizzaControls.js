f60b1083bcdd7ac7b61f53bbb062198ef6d86ab5
Your Name <you@example.com>
2021-03-26 14:33:31 +0600

    create Pizza Controls compinent

---
+++ src/components/PizzaBuilder/PizzaControls/PizzaControls.js
@@ -0,0 +1,14 @@
+import classes from "./PizzaControls.module.css";
+
+const PizzaControls  = ({ ingredients}) => {
+    const results = [];
+    for (const ingredient in ingredients) {
+        results.push(ingredient);
+    }
+
+    return ( 
+    <div className={classes.PizzaControls}>results</div> 
+    );
+}
+ 
+export default PizzaControls ;

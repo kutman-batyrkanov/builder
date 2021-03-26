be670438a5772116b6c211ad2bc5b1832a858108
Your Name <you@example.com>
2021-03-26 15:38:42 +0600

    added Pizza ingredient component in Pizza buider

--- src/components/PizzaBuilder/PizzaBuilder.js
+++ src/components/PizzaBuilder/PizzaBuilder.js
@@ -1,19 +1,35 @@
 import classes from "./PizzaBuilder.module.css";
 import { useState } from "react";
 import PizzaControls from "./PizzaControls/PizzaControls";
+import PizzaPreview from "./PizzaPreview/PizzaPreview";
+import PizzaControl from "./PizzaControls/PizzaControl/PizzaControl";
 
 const PizzaBulider = () => {
     const [ingredients, setIngredients] = useState ({
-        tomato: 20,
-        salami: 20,
-        greenOlive: 20,
-        blackOlive: 20,
-        redPepper: 20,
-        yellowPepper: 15,
-    })
+        tomato: 1,
+        salami: 1,
+        greenOlive: 1,
+        blackOlive: 1,
+        redPepper: 1,
+        yellowPepper: 1,
+    });
+
+    function addIngredient(type) {
+        const newIngredients = { ...ingredients };
+        newIngredients[type]++;
+        setIngredients(newIngredients);
+    }
+    
+    function removeIngredient(type) {
+        const newIngredients = { ...ingredients };
+        newIngredients[type]--;
+        setIngredients(newIngredients);
+    }
     return ( 
     <div className={classes.PizzaBulider}>
-        <PizzaControls ingredients={ingredients} />
+        <PizzaControls ingredients={ingredients} addIngredient={addIngredient} removeIngredient={removeIngredient} />
+        <PizzaPreview ingredients={ingredients} />
+        <PizzaControl />
     </div> 
     );
 }

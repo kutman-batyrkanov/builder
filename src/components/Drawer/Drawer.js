bc8b05805c02a607a65a52ccace0d74289499aaf
Your Name <you@example.com>
2021-03-26 15:36:16 +0600

    fixed component Drawer js and Drawe css

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,10 +1,9 @@
-import classes from "./Drawer.module.css";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import Backdrop from "../UI/Backdrop/Backdrop";
+import classes from "./Drawer.module.css";
 
-const Drawer = ({}) => {
-
+const Drawer = ({ open, closeDrawer }) => {
   const drawerClasses = [
     classes.content,
     open ? classes.open : classes.close
@@ -12,13 +11,13 @@ const Drawer = ({}) => {
 
   return (
     <div className={classes.Drawer}>
-        <Backdrop show={open} click={closeDrawer} />
+      <Backdrop show={open} click={closeDrawer} />
       <div className={drawerClasses.join(" ")}>
         <Logo />
         <Nav />
       </div>
     </div>
   );
-};
-
+}
+ 
 export default Drawer;

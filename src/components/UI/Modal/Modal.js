ad9b29698d7dd59bba7a7de918db0f487cec9237
Your Name <you@example.com>
2021-04-07 15:45:22 +0600

    add transform show translate Y and tanslate Y -100vh

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,19 @@
+import classes from "./Modal.module.css";
+import Backdrop from "../Backdrop/Backdrop";
+
+const Modal = ({ show, children, cancel }) => {
+  const style = {
+    transform: show ? "translateY(0)" : "translateY(-100vh)",
+  };
+
+  return (
+    <div className={classes.Modal}>
+      <Backdrop show={show} click={cancel} />
+      <div className={classes.content} style={style}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
